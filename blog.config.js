2998366650fc42dd63f808d744e87893cd849706
Faiz S. Hussain <faizsyedhussain@gmail.com>
2021-07-26 16:48:13 +0100

    Line edits to Config

--- blog.config.js
+++ blog.config.js
@@ -10,7 +10,7 @@ const BLOG = {
         lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
         darkBackground: '#18181B', // use hex value, don't forget '#'
         path: '', // leave this empty unless you want to deploy Nobelium in a folder
-        since: , // if leave this empty, current year will be used.
+        since: '', // if leave this empty, current year will be used.
         postsPerPage: 7,
         sortByDate: true,
         showAbout: true, // WIP
