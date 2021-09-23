7e1cf5c970176dd5c0c43390e0360669bfcd6c1f
Faiz S. Hussain <faizsyedhussain@gmail.com>
2021-09-23 23:43:49 +0200

    Reformat Footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,18 +7,29 @@ const Footer = ({ fullWidth }) => {
         div className = { `mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
         !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
       }` } >
-        <
-        hr className = "border-gray-200 dark:border-gray-600" / >
+        
         <
         div className = "my-4 text-sm leading-6 font-normal" >
         <
         div className = "flex align-baseline justify-between flex-wrap" >
         <
-        p > <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY&middot;NC&middot;SA</a> &mdash;&nbsp;{ BLOG.author } { from === y || !from ? y : `${from}–${y}` } <
-        /p> < /
-        div > <
-        /div> < /
+        div className = "grid grid-cols-3 gap-4" >
+        <
         div >
+        <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY&middot;NC&middot;SA</a><
+        /div>
+        <
+        div >
+        { BLOG.author } <
+        /div>
+        <
+        div >
+        { from === y || !from ? y : `${from}–${y}` } <
+        < 
+        /div > <
+        /div > <
+        /div > <
+        /div >
   )
 }
 
