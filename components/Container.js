4736652d0d4c2a6f62481753aa73881b44b4aab2
Faiz S. Hussain <faizsyedhussain@gmail.com>
2021-12-28 22:23:46 +0000

    Aligning margins

--- components/Container.js
+++ components/Container.js
@@ -68,8 +68,6 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
       >
         <Header
           navBarTitle={layout === 'blog' ? meta.title : null}
-          className={`m-auto flex-grow w-full transition-all ${
-            !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
           }`}
         />
         <main
