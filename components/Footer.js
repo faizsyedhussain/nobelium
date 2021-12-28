8c062fd731ea3ac2f7b0d7db775dcbcdce650488
Faiz S. Hussain <faizsyedhussain@gmail.com>
2021-12-28 19:30:49 +0000

    Cleaned up footer errors

--- components/Footer.js
+++ components/Footer.js
@@ -8,25 +8,13 @@ const Footer = ({ fullWidth }) => {
         !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
       }` } >
         
-        <
-        div className = "my-4 text-sm leading-6 font-normal" >
-        <
-        div className = "flex align-baseline justify-between flex-wrap" >
-        <
-        div className = "grid grid-cols-3 gap-4" >
-        <
-        div >
-        <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY&middot;NC&middot;SA</a><
-        /div>
-        <
-        div >
-        { BLOG.author } <
-        /div>
-        <
-        div >
-        { from === y || !from ? y : `${from}–${y}` } <
-        < 
-        /div > <
+        <div className = "my-4 text-sm leading-6 font-normal" >
+        <div className = "flex align-baseline justify-between flex-wrap" >
+        <div className = "grid grid-cols-3 gap-4" >
+        <div><a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY&middot;NC&middot;SA</a></div>
+        <div>{ BLOG.author } </div>
+        <div>{ from === y || !from ? y : `${from}–${y}` }
+        </div > <
         /div > <
         /div > <
         /div >
