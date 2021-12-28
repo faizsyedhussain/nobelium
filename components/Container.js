4b8e534aed9724ac0071547048b83979c53593ce
Faiz S. Hussain <faizsyedhussain@gmail.com>
2021-12-28 21:15:43 +0000

    Sync header width with text

--- components/Container.js
+++ components/Container.js
@@ -68,7 +68,9 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
       >
         <Header
           navBarTitle={layout === 'blog' ? meta.title : null}
-          fullWidth={fullWidth}
+          className={`m-auto flex-grow w-full transition-all ${
+            !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
+          }`}
         />
         <main
           className={`m-auto flex-grow w-full transition-all ${
