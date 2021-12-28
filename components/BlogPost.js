738001b82ef101715963b892c06aebb07f657e63
Faiz S. Hussain <faizsyedhussain@gmail.com>
2021-12-28 21:31:03 +0000

    Fixed leading issue

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -18,7 +18,7 @@ const BlogPost = ({ post }) => {
         </time>
       </header>
       <main>
-        <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
+        <p className="hidden md:block leading-r2 text-gray-700 dark:text-gray-300">
           {post.summary}
         </p>
       </main>
