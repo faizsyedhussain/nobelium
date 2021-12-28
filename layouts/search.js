a44e0a49b4711ffcf2034f9902d9e12bfd6840d3
Faiz S. Hussain <faizsyedhussain@gmail.com>
2021-12-28 22:41:05 +0000

    Search again

--- layouts/search.js
+++ layouts/search.js
@@ -23,7 +23,7 @@ const SearchLayout = ({ tags, posts, currentTag }) => {
           placeholder={
             currentTag ? `Search in #${currentTag}` : 'Search Articles'
           }
-          className="block w-full border px-4 py-2 border-black bg-gray-700 text-black dark:bg-night dark:border-white dark:text-white"
+          className="block w-full border px-4 py-2 border-gray-700 bg-transparent text-black dark:bg-night dark:border-white dark:text-white"
           onChange={e => setSearchValue(e.target.value)}
         />
         <svg
