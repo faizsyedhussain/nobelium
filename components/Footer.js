ee4e5cf96335d0a144ed879018cd7c50ea18940b
Faiz S. Hussain <faizsyedhussain@gmail.com>
2021-12-28 20:50:12 +0000

    Fixed footer and scripts again from source

--- components/Footer.js
+++ components/Footer.js
@@ -1,23 +1,25 @@
 import BLOG from '@/blog.config'
+import Vercel from '@/components/Vercel'
 const Footer = ({ fullWidth }) => {
   const d = new Date()
   const y = d.getFullYear()
   const from = +BLOG.since
-  return (<
-        div className = { `mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
+  return (
+    <div
+      className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
         !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
-      }` } >
-        
-        <div className = "my-4 text-sm leading-6 font-normal" >
-        <div className = "flex align-baseline justify-between flex-wrap" >
-        <div className = "grid grid-cols-3 gap-4" >
-        <div><a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY&middot;NC&middot;SA</a></div>
-        <div>{ BLOG.author } </div>
-        <div>{ from === y || !from ? y : `${from}–${y}` }
-        </div > <
-        /div > <
-        /div > <
-        /div >
+      }`}
+    >
+      <hr className="border-gray-200 dark:border-gray-600" />
+      <div className="my-4 text-sm leading-6">
+        <div className="flex align-baseline justify-between flex-wrap">
+          <p>
+            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+          </p>
+          <Vercel />
+        </div>
+      </div>
+    </div>
   )
 }
 
